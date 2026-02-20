bc45ce0c8dcd9c689239a2ca2ae814e6695becd2
Serveone-Tech <lokendrasingh271193@gmail.com>
2026-02-20 23:12:42 +0530

    test deployment

--- src/components/auth/SignIn/SignIn.tsx
+++ src/components/auth/SignIn/SignIn.tsx
@@ -38,7 +38,7 @@ const SignIn = ({
                 />
             </div>
             <div className="mb-10">
-                <h2 className="mb-2">Welcome back!</h2>
+                <h2 className="mb-2">Welcome to goa singham!</h2>
                 <p className="font-semibold heading-text">
                     Please enter your credentials to sign in!
                 </p>
